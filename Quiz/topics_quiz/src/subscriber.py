167601cf1a9a3d219a1bb1fce643a29b2f3da112
C9Cubz <rogozageorge@yahoo.com>
2023-04-03 12:54:50 +0300

    quiz

--- Quiz/topics_quiz/src/subscriber.py
+++ Quiz/topics_quiz/src/subscriber.py
@@ -1,10 +1,26 @@
 #! /usr/bin/env python
 import rospy
-from nav_msgs.msg import Odometry
-def callback(msg):
-	print(msg.data)
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
-rospy.init_node('topic_subscriber')
-sub=rospy.Subscriber('/scan',Odometry,callback)
+def callback(sensor_msgs):
+    print (sensor_msgs.ranges)
+
+rospy.init_node('topic_subscriber')  
+sub = rospy.Subscriber('/scan', LaserScan, callback)
+rospy.spin()
+
+pub=rospy.Publisher('/cmd_vel',Twist,queue_size=1)
+rate=rospy.Rate(2)
+
+count = Twist()
+count.linear.x=1
+count.angular.z=0
+
+while not rospy.is_shutdown():
+	if sensor_msgs.ranges >0:
+	pub.publisher(count)
+	count.angular.z +=0.5
+	rate.sleep()
 
 rospy.spin()
