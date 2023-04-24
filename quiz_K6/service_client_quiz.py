43e8a3ced5ff05ebc0b9c15288decc9a92688aee
C9Cubz <rogozageorge@yahoo.com>
2023-04-24 15:19:07 +0300

    all

--- quiz_K6/service_client_quiz.py
+++ quiz_K6/service_client_quiz.py
@@ -7,15 +7,15 @@ import sys
 # Initialise a ROS node with the name service_client
 rospy.init_node('service_client_quiz')
 # Wait for the service client /move_in_square to be running
-rospy.wait_for_service('/move_tb_in_square')
-time= rospy.get_param('time')
+rospy.wait_for_service('/move_in_square')
 # Create the connection to the service
-move_tb_in_circle_service = rospy.ServiceProxy('/move_tb_in_square', CustomServMess)
+move_in_circle_service = rospy.ServiceProxy('/move_in_square', CustomServMess)
 # Create an object of type MoveInSquareRequest
-move_tb_in_circle_object = CustomServMessRequest()
-move_tb_in_circle_object.duration=10
+move_in_circle_object = CustomServMessRequest()
+move_in_circle_object.side=2
+move_in_circle_object.repetition=2
   
 # Send through the connection the name of the request
-result = move_tb_in_circle_service(move_tb_in_circle_object)
+result = move_in_circle_service(move_in_circle_object)
 # Print the result given by the service called
 print(result)
